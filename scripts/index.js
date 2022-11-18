d2d5196ae8a4ceef55250f4774a4660a85b81b74
artaleal <artaleal@yandex.ru>
2022-11-18 22:18:41 +0300

    develop JS touch

---
+++ scripts/index.js
@@ -0,0 +1,7 @@
+console.log('hello')
+const slider = document.querySelector('.slider__row');
+const slides = slider.querySelectorAll('.slider__item');
+const prevSlide = slider.querySelector('.slider__button_prev');
+const nextSlide = slider.querySelector('.slider__button_next');
+
+console.log(slider, slides, prevSlide, nextSlide);
