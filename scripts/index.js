93e7a7605b07ebd9032198b05c43a41c481f477d
artaleal <artaleal@yandex.ru>
2022-11-18 22:46:45 +0300

    js not work correctly

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,36 @@
-console.log('hello')
 const slider = document.querySelector('.slider__row');
 const slides = slider.querySelectorAll('.slider__item');
 const prevSlide = slider.querySelector('.slider__button_prev');
 const nextSlide = slider.querySelector('.slider__button_next');
 
-console.log(slider, slides, prevSlide, nextSlide);
+let state = 0;
+let step = slides[0].clientWidth;
+
+nextSlide.addEventListener('click', () => {
+  slides.forEach(slider => {
+    slider.setAttribute('style', `transform: translate3d(${state - step}px, 0, 0)`);
+    console.log(state);
+  })
+  state = state - step;
+
+  if (state < 0) {
+    nextSlide.setAttribute('disabled', '');
+    prevSlide.removeAttribute('disabled', '');
+  }
+});
+
+prevSlide.addEventListener('click', () => {
+  slides.forEach(slider => {
+    slider.setAttribute('style', `transform: translate3d(${state + step}px, 0, 0)`);
+    console.log(state);
+  })
+
+  state += step;
+
+  if (state > 0) {
+    prevSlide.setAttribute('disabled', '');
+    nextSlide.setAttribute('disabled', '');
+  }
+})
+
+
